4c0d348a026aa2638ee844e60e8b56ca01e38a83
laidongyi <695163222@qq.com>
2018-01-04 01:52:37 +0800

    Delete enroll.js

--- enroll.js
+++
@@ -1,7 +0,0 @@
-var enroll = document.getElementById("enroll");
-		      enroll.onclick =function(e){
-		           e.preventDefault();
-			       enroll.innerHTML = "我来了";
-			       enroll.style.background = "#27cb8b";
-			       enroll.style.borderColor = "#27cb8b";
-		      }
